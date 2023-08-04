93ea499b2cbf4f204ad1d72d22c35f42554274d5
Shreyas Patil <110882047+shreefsd@users.noreply.github.com>
2023-08-04 17:59:39 +0530

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,26 +3,20 @@ import React, { useState, useEffect } from 'react';
 function Top() {
     const [cryptoArr, setCryptoArr] = useState([]);
     useEffect(() => {
-        fetch('https://api.coinlore.net/api/tickers/?start=0&limit=10')
-        .then((resp) => resp.json)
-        .then((data) => {
-            setCryptoArr(data.data);
-        })
-        .catch((error) => {
-        console.error(error);
-      });
+        async function fetchData(){
+         let info = await fetch('https://api.coinlore.net/api/tickers/?start=0&limit=10')
+         info = await info.json()
+         setCryptoArr(info.data)
+      }
+      fetchData();
     },[]);
-
-    if (!cryptoArr || cryptoArr.length === 0) {
-        return <div className='top-container'><h1>Top 10 Global Crypto Coins</h1></div>;
-    }
     
     return (
         <div className='top-container'>
             <h1>Top 10 Global Crypto Coins</h1>
             <div className='display-container'>
-                {cryptoArr.map((ele) => (
-                <div className='coin-container'>
+                {cryptoArr.map((ele,i) => (
+                <div className='coin-container'key={i}>
                     <p className='rank-para'>Rank #{ele.rank}</p>
                     <h3 className='coin-symbol'>{ele.name} ({ele.symbol})</h3>
                     <p className='price-para'>Price: {ele.price_usd}</p>
